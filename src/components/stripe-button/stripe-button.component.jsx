6c6aa048dfb1826753a092031a74053a12676174
José Rosario-López <jose@sooryen.com>
2020-06-23 01:13:15 -0400

    updating Stripe integration; updating favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51Gx3ARD4k3P78Do6BKM0uqVq2rya8342ZpQQuCjr67FvvchbeJoHAKi8KRAePjKD35MiRMCHubJbe1fC5KLNM9Ff009hNysVtr';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
+
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import './checkout.styles.scss';
 
@@ -29,9 +31,35 @@ const CheckoutPage = ({ cartItems, total }) => (
             cartItems.map(cartItem =>
                 <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
         }
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
+        <div className='total'>TOTAL: ${total}</div>
+
+        <div className='test-warning'>
+            *Please use the following test credit cards for payments*
+            <br />
+            <table id="test-checkout">
+                <thead>
+                    <tr>
+                        <td>Brand</td><td>Number</td><td>Exp. Date</td><td>CVC</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Visa</td><td>4242 4242 4242 4242</td><td>Any future date</td><td>123</td>
+                    </tr>
+                    <tr>
+                        <td>Master Card</td><td>5555 5555 5555 4444</td><td>Any future date</td><td>123</td>
+                    </tr>
+                    <tr>
+                        <td>American Express</td><td>3782 822463 10005</td><td>Any future date</td><td>1234</td>
+                    </tr>
+                    <tr>
+                        <td>Discover</td><td>6011 1111 1111 1117</td><td>Any future date</td><td>123</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
+
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
